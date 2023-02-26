99fa3cdf585378530bc97c7d133d500a1f38c627
Lera <v.osh.frrr@gmail.com>
2023-02-26 21:29:18 +0300

    выделен класс для управления контактами

---
+++ fixture/contact.py
@@ -0,0 +1,47 @@
+import os
+from selenium.webdriver.support.ui import Select
+
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def create(self, contact):
+        wd = self.app.wd
+        # init add contact
+        wd.find_element_by_link_text("add new").click()
+        # add contact
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("photo").send_keys(os.path.join('C:\Learning\Python_ST_2\python_ST\image',
+                                                                contact.image_name))
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").send_keys(contact.home_number)
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_number)
+        wd.find_element_by_name("work").send_keys(contact.work_number)
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("email2").send_keys(contact.email2)
+        wd.find_element_by_name("email3").send_keys(contact.email3)
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        wd.find_element_by_xpath("//option[@value='16']").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element_by_xpath("//option[@value='November']").click()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aday)
+        wd.find_element_by_xpath("//div[@id='content']/form/select[3]/option[20]").click()
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
+        wd.find_element_by_xpath("//div[@id='content']/form/select[4]/option[13]").click()
+        wd.find_element_by_name("ayear").send_keys(contact.ayear)
+        wd.find_element_by_name("address2").send_keys(contact.address2)
+        wd.find_element_by_name("phone2").send_keys(contact.phone2)
+        wd.find_element_by_name("notes").send_keys(contact.notes)
+        # submit
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.app.return_to_home_page()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -14,15 +14,15 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(login="admin", password="secret")
-    app.add_contact(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov",
-                            nickname="vanek", image_name="test_image.jpg", title="example",
-                            company="berc", address="Saint Petersburg, Nevskiy Avenue",
-                            home_number="12345678", mobile_number="1234567", work_number="123456789",
-                            fax="123", email="exampleq@mail.ru", email2="example2@mail.ru",
-                            email3="example3@mail.ru", homepage="google.com", bday="16",
-                            bmonth="November", byear="1996", aday="18", amonth="December",
-                            ayear="2000", address2="Saint Petersburg, Sadovaya Street",
-                            phone2="Saint Petersburg", notes="lalalala"))
+    app.contact.create(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov",
+                               nickname="vanek", image_name="test_image.jpg", title="example",
+                               company="berc", address="Saint Petersburg, Nevskiy Avenue",
+                               home_number="12345678", mobile_number="1234567", work_number="123456789",
+                               fax="123", email="exampleq@mail.ru", email2="example2@mail.ru",
+                               email3="example3@mail.ru", homepage="google.com", bday="16",
+                               bmonth="November", byear="1996", aday="18", amonth="December",
+                               ayear="2000", address2="Saint Petersburg, Sadovaya Street",
+                               phone2="Saint Petersburg", notes="lalalala"))
     app.session.logout()
 
 
