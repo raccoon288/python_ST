097ee2a602cc14f3c085a27d3c1a8265b25ed5c9
Lera <v.osh.frrr@gmail.com>
2023-03-02 22:12:21 +0300

    добавлено предусловие в тестах на изменение группы

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,12 +2,18 @@ from model.group import Group
 
 
 def test_modify_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first(Group(name="Hohoho"))
 
 
 def test_modify_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first(Group(header="Hohoho"))
 
 
 def test_modify_first_group_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first(Group(footer="Hohoho"))
