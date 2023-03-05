8c7985e5ba26768ac6f864dc6e754383938b703f
Lera <v.osh.frrr@gmail.com>
2023-03-05 21:45:25 +0300

    добавлены метод получения списка групп и проверки на длину списка в тестах для групп

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,6 @@
+from model.group import Group
+
+
 class GroupHelper:
 
     def __init__(self, app):
@@ -28,6 +31,17 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_group_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id=id))
+        return groups
+
 
     def create(self, group):
         wd = self.app.wd
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,16 +4,25 @@ from model.group import Group
 def test_modify_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first(Group(name="Hohoho"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_first_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first(Group(header="Hohoho"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_first_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first(Group(footer="Hohoho"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
