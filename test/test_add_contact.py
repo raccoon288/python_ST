7965f997057ed17cc0a61eeff7d567ab32508d03
Lera <v.osh.frrr@gmail.com>
2023-03-11 18:37:22 +0300

    кеширование для контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -14,8 +14,8 @@ def test_add_contact(app):
                       ayear="2000", address2="Saint Petersburg, Sadovaya Street",
                       phone2="Saint Petersburg", notes="la")
     app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
@@ -32,8 +32,8 @@ def test_add_empty_contact(app):
                       ayear="", address2="",
                       phone2="", notes="")
     app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
