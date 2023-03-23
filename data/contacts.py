958c70d6811c83aa0c1f0c02cad0423e180f8aa6
Lera <v.osh.frrr@gmail.com>
2023-03-23 22:01:26 +0300

    реализована параметризация тестов с помощью тестовых данных из файла формата json

--- data/contacts.py
+++ data/contacts.py
@@ -1,33 +1,4 @@
 from model.contact import Contact
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits
-    random_len = random.randrange(maxlen)
-    random_line = [random.choice(symbols) for i in range(random_len)]
-    return prefix + "".join(random_line)
-
-
-def random_phones(prefix, maxlen):
-    symbols = string.digits + "[" + "(" + ")" + "-" + "]"
-    random_len = random.randrange(maxlen)
-    random_line = [random.choice(symbols) for i in range(random_len)]
-    return prefix + "".join(random_line)
-
-
-def random_emails(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + "-" + "_"
-    random_len = random.randrange(maxlen)
-    random_line = [random.choice(symbols) for i in range(random_len)]
-    return prefix + "".join(random_line) + "@" + "".join(random_line) + ".ru"
-
-
-def random_images():
-    images = ["test_image.jpg", "test_image2.jpg", "test_image3.jpg", "test_image4.jpg", "test_image5.jpg"]
-    index = random.randrange(5)
-    return images[index]
 
 
 testdata = [
@@ -55,33 +26,31 @@ testdata = [
                     ayear="",
                     address2="",
                     phone2="",
-                    notes="")
-            ] + \
-            [
-             Contact(firstname=random_string("firstname", 10),
-                     middlename=random_string("middlename", 10),
-                     lastname=random_string("lastname", 10),
-                     nickname=random_string("nickname", 10),
-                     image_name=random_images(),
-                     title=random_string("title", 10),
-                     company=random_string("company", 10),
-                     address=random_string("address", 10),
-                     home_number=random_phones("home_number", 15),
-                     mobile_number=random_phones("mobile_number", 15),
-                     work_number=random_phones("work_number", 15),
-                     fax=random_phones("fax", 15),
-                     email=random_emails("email", 10),
-                     email2=random_emails("email2", 10),
-                     email3=random_emails("email3", 10),
-                     homepage=random_string("homepage", 10),
-                     bday=random.randrange(1, 32),
-                     bmonth=random.randrange(1, 13),
-                     byear=random.randrange(1, 3000),
-                     aday=random.randrange(1, 32),
-                     amonth=random.randrange(1, 13),
-                     ayear=random.randrange(1, 3000),
-                     address2=random_string("address2", 10),
-                     phone2=random_phones("phone2", 15),
-                     notes=random_string("notes", 10))
-             for i in range(5)
+                    notes=""),
+
+            Contact(firstname="firstname1",
+                    middlename="middlename1",
+                    lastname="lastname1",
+                    nickname="nickname1",
+                    image_name="test_image.jpg",
+                    title="title1",
+                    company="company1",
+                    address="address1",
+                    home_number="123",
+                    mobile_number="456",
+                    work_number="789",
+                    fax="098",
+                    email="email1",
+                    email2="email2",
+                    email3="email3",
+                    homepage="homepage1",
+                    bday="1",
+                    bmonth="2",
+                    byear="1990",
+                    aday="3",
+                    amonth="4",
+                    ayear="2000",
+                    address2="address2",
+                    phone2="365",
+                    notes="notes1")
             ]
